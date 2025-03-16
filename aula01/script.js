4d05eeff60c041db00c1f197421dcc1bc4ae94f1
IsabelMelo4 <isabelmelo630@gmail.com>
2025-03-16 18:48:22 -0300

    aula de DOOM

---
+++ aula01/script.js
@@ -0,0 +1,4 @@
+var p1 = window.document.getElementsByTagName('p')[0]
+window.document.write(p1.innerText)
+
+var d = window.document.getElementById('msg')
---
+++ aula02/script.js
@@ -0,0 +1,19 @@
+
+
+var a = window.document.getElementById('area')
+
+
+function clicar(){
+    a.innerHTML = 'clicou'
+    a.style.background = 'yellow'
+}
+
+function entrar(){
+    a.innerHTML = 'entrou'
+    a.style.background = 'blue'
+}
+
+function sair(){
+    a.innerHTML = 'saiu'
+    a.style.background = 'red'
+}
